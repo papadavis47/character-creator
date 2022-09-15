6845ccfee3837c0f0d6d140dd372c9ef71d207a2
John William Davis <jwdavisdeveloper@gmail.com>
2022-09-15 08:49:42 -0700

    Added changes after watching solutions again

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,8 +30,8 @@ function App() {
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the controls below.
-            What sort of adventure will you embark on?{" "}
+            Customize your character's look and style using the controls below. What sort of
+            adventure will you embark on?{" "}
           </p>
         </header>
         <div className={styles.controlColumn}>
